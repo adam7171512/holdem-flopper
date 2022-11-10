3a0956fc48f2a46561a1b6c9aedaee9b65548dde
adam7 <adam7171512@gmail.com>
2022-11-10 17:19:55 +0100

    added add_filter method

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-# Import module
 from tkinter.scrolledtext import ScrolledText
 import tkinter as tk
 from tkinter import ttk
@@ -24,7 +23,7 @@ class FlopperGui(tk.Tk):
         self.texture_options = texture_labels
         self.texture_clicked = tk.StringVar(self)
         self.logical_operator_clicked = tk.StringVar(self)
-        self.filter_negation_clicked = tk.BooleanVar(self)
+        self.filter_negation_clicked = tk.StringVar(self)
 
 
         self.ranks_clicked = tk.StringVar()
@@ -34,7 +33,7 @@ class FlopperGui(tk.Tk):
         self.ranks_card_clicked = tk.StringVar()
         self.ranks_card_button_enabled = 0
 
-        self.and_or_options = ["AND", "OR"]
+        self.and_or_options = ["AND ", "OR "]
 
         self.filter_field_text = tk.StringVar(self)
         self.filter_field = tk.Text(self, width=20, height=10)
@@ -63,16 +62,16 @@ class FlopperGui(tk.Tk):
         logical_label = tk.Label(logical_frame, text="Select logical operator", font=12)
         logical_label.grid(column=1, row=1, columnspan=2)
 
-        and_or_dropdown = ttk.OptionMenu(logical_frame, self.logical_operator_clicked, "AND", *self.and_or_options)
+        and_or_dropdown = ttk.OptionMenu(logical_frame, self.logical_operator_clicked, "AND ", *self.and_or_options)
         and_or_dropdown.grid(column=1, row=2, padx=10, pady=2, columnspan=2)
 
         negation_label = tk.Label(logical_frame, text="Click to negate the filter", font=12)
         negation_label.grid(column=1, row=3, columnspan=2)
 
-        negation_box = ttk.Checkbutton(logical_frame, text="NOT", variable=self.filter_negation_clicked, onvalue=1, offvalue=0)
+        negation_box = ttk.Checkbutton(logical_frame, text="NOT", variable=self.filter_negation_clicked, onvalue="NOT ", offvalue="")
         negation_box.grid(column=1, row=4, columnspan=2)
 
-        add_filter_button = ttk.Button(self, text="ADD FILTER", command="")
+        add_filter_button = ttk.Button(self, text="ADD FILTER", command=self.add_filter)
         add_filter_button.grid(column=1, row=3, pady=5)
 
         self.filter_field.grid(column=1, row=4)
@@ -89,6 +88,18 @@ class FlopperGui(tk.Tk):
 
         flop_list_field = ScrolledText(flop_list_frame, width=20, height=30)
         flop_list_field.grid(column=2, row=1, padx=2, rowspan=4)
+
+    def add_filter(self):
+        if "card" not in self.texture_clicked.get():
+            if len(self.filter_field.get('0.0', 'end')) < 2:
+                self.filter_field.insert('end', self.filter_negation_clicked.get() + self.texture_clicked.get() + "\n")
+            else:
+                self.filter_field.insert('end', self.logical_operator_clicked.get() + self.filter_negation_clicked.get()  + self.texture_clicked.get() + "\n")
+        else:
+            if len(self.filter_field.get('0.0', 'end')) < 2:
+                self.filter_field.insert('end', self.filter_negation_clicked.get() + self.texture_clicked.get()  + " " + self.ranks_card_clicked.get() + "\n")
+            else:
+                self.filter_field.insert('end', self.logical_operator_clicked.get() + self.filter_negation_clicked.get()  + self.texture_clicked.get() + " " + self.ranks_card_clicked.get() + "\n")
 
 
 gui = FlopperGui()
